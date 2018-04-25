fd593db4181a4a0c20d0c68a6607152174fb707a
Cam Button and Garnett Puett <cam@>
2018-04-25 15:25:53 -0700

    Add css styling.

--- src/api-call.js
+++ src/api-call.js
@@ -3,12 +3,13 @@ import $ from 'jquery';
 class Api {
   call(character) {
     $.get(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${character}&apikey=${process.env.API_KEY}`).then(function(response) {
-      $('.container').append(`<div><img src="${response.data.results[0].thumbnail.path}.jpg"><p>${response.data.results[0].description}</p></div>`);
+      $('.character').html(`<div><img src="${response.data.results[0].thumbnail.path}.jpg"><p>${response.data.results[0].description}</p></div>`);
     }).fail(function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
+    let random = Math.floor(Math.random() * (20 - 0));
     $.get(`https://gateway.marvel.com:443/v1/public/comics?titleStartsWith=${character}&apikey=${process.env.API_KEY}`).then(function(response) {
-      $('.container').append(`<div><img src="${response.data.results[0].thumbnail.path}.jpg"><p>${response.data.results[0].description}</p></div>`);
+      $('.comic').html(`<div><img src="${response.data.results[random].thumbnail.path}.jpg"><p>${response.data.results[random].description}</p></div>`);
     }).fail(function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
