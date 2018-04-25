2f69614b7301922d0b6e27fec21ac77f53a0bd01
Cam Button and Garnett Puett <cam@>
2018-04-25 14:48:22 -0700

    add first result of comic appearance.

--- src/api-call.js
+++ src/api-call.js
@@ -7,6 +7,11 @@ class Api {
     }).fail(function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
+    $.get(`https://gateway.marvel.com:443/v1/public/comics?titleStartsWith=${character}&apikey=${process.env.API_KEY}`).then(function(response) {
+      $('.container').append(`<div><img src="${response.data.results[0].thumbnail.path}.jpg"><p>${response.data.results[0].description}</p></div>`);
+    }).fail(function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+    });
   }
 }
 
